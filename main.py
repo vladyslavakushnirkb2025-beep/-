7f8b8250e6172dd686d5e5e8aa3c8e63add9a544
Vlada <vladakushnir888@gmail.com>
2026-09-21 21:17:20 +0300

    Add main entry point

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from lib import check_password_length, format_user_data
-def main():
+from lib import format_username, validate_password_length
+
+
+def main() -> None:
+    """
+    Головна функція запуску скрипту.
+    """
+    user_name = "  Admin_User "
     test_password = "mySecretPassword123"
-    test_user = "  Admin_User  "
-    # Викликаємо функції з модуля lib
-    is_valid = check_password_length(test_password)
-    formatted_name = format_user_data(test_user)
-    # Виводимо результати
+
+    formatted_user = format_username(user_name)
+    is_valid = validate_password_length(test_password)
+
     print("--- Результати виконання скрипту ---")
-    print(f"Користувач: {formatted_name}")
+    print(f"Користувач: {formatted_user}")
     print(f"Чи надійний пароль '{test_password}' за довжиною: {is_valid}")
+
+
 if __name__ == "__main__":
     main()
